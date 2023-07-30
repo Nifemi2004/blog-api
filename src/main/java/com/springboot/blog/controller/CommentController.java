0676ae1eaad89791c10a9e1fcc54f72d7c078e2c
SokooyaN <nifemi.sokooya@centrifugegroup.com>
2023-07-30 22:06:28 +0100

    Spring Security added

--- src/main/java/com/springboot/blog/controller/CommentController.java
+++ src/main/java/com/springboot/blog/controller/CommentController.java
@@ -2,6 +2,7 @@ package com.springboot.blog.controller;
 
 import com.springboot.blog.payload.CommentDto;
 import com.springboot.blog.service.CommentService;
+import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -17,7 +18,8 @@ public class CommentController {
     }
 
     @PostMapping("/posts/{postId}/comments")
-    public ResponseEntity<CommentDto> createComment(@PathVariable(value = "postId") long postId, @RequestBody CommentDto commentDto){
+    public ResponseEntity<CommentDto> createComment(@PathVariable(value = "postId") long postId,
+                                                    @Valid @RequestBody CommentDto commentDto){
         return new ResponseEntity<>(commentService.createComment(postId, commentDto), HttpStatus.CREATED);
     }
 
@@ -27,12 +29,15 @@ public class CommentController {
     }
 
     @GetMapping("/posts/{postId}/comments/{id}")
-    public ResponseEntity<CommentDto> getCommentById(@PathVariable(value = "postId") long postId, @PathVariable(value = "id") long commentId){
+    public ResponseEntity<CommentDto> getCommentById(@PathVariable(value = "postId") long postId,
+                                                     @PathVariable(value = "id") long commentId){
         return new ResponseEntity<>(commentService.getCommentById(postId, commentId), HttpStatus.OK);
     }
 
     @PutMapping("/posts/{postId}/comments/{id}")
-    public ResponseEntity<CommentDto> updateComment(@PathVariable(value = "postId") long postId,@PathVariable(value = "id") long commentId,@RequestBody CommentDto commentDto){
+    public ResponseEntity<CommentDto> updateComment(@PathVariable(value = "postId") long postId,
+                                                    @PathVariable(value = "id") long commentId,
+                                                    @Valid @RequestBody CommentDto commentDto){
         CommentDto updatedPost = commentService.updateComment(postId, commentId, commentDto);
 
         return new ResponseEntity<>(updatedPost, HttpStatus.OK);
--- src/main/java/com/springboot/blog/payload/CommentDto.java
+++ src/main/java/com/springboot/blog/payload/CommentDto.java
@@ -1,11 +1,23 @@
 package com.springboot.blog.payload;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 
 @Data
 public class CommentDto {
     private Long id;
+
+    @NotEmpty(message = "cannot be empty")
+    @Size(min = 3)
     private String name;
+
+    @NotEmpty(message = "cannot be empty")
+    @Email
     private String email;
+
+    @NotEmpty(message = "cannot be empty")
+    @Size(min = 10)
     private String body;
 }
